b20584c7ae9f38b44cc686b23af0e9063e1f8371
Sofia Milhomem <sofialeitemilhomem@gmail.com>
2025-06-26 01:13:20 +0000

    exibição do cadastro das cartas 1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -36,8 +36,8 @@ int main() {
 
 
     //saudações para começar o desafio
-    printf("\n -- SUPER TRUNFO --\n");
-    printf("\n - CADASTRAMENTO DAS CARTAS -\n");
+    printf("\n --------- SUPER TRUNFO ---------\n");
+    printf("\n ---- CADASTRAMENTO DAS CARTAS ----\n");
     
     //CADASTRAMENTO - para o usuario inserir as infos da carta 1
 printf("\n º CADASTRO DA CARTA 1 º\n");
@@ -49,7 +49,7 @@ printf("- INFORME O CÓDIGO DA CARTA (EX.: A01, 102, B01...):\n");
 scanf("%s", codigo1);
 
 printf("- INFORME O NOME DA CIDADE (SEM ESPAÇOS): \n");
-scanf("%s", &cidade1);
+scanf("%s", cidade1);
 
 printf("- INFORME O NÚMERO DA POPULAÇÃO(SEM PONTOS OU VÍRGULAS):\n");
 scanf("%d", &populacao1);
@@ -71,10 +71,10 @@ printf(" - INFORME UMA LETRA PARA REPRESENTAR O ESTADO (DE A a H):\n");
 scanf(" %c", &estado2);
 
 printf("- INFORME O CÓDIGO DA CARTA (EX.: A01, 102, B01...):\n");
-scanf("%s", codigo2);
+scanf("%s", codigo2); 
 
 printf(" - INFORME O NOME DA CIDADE (SEM ESPAÇOS):\n");
-scanf("%s", &cidade2);
+scanf("%s", cidade2);
 
 printf("- INFORME O NÚMERO DA POPULAÇÃO:\n");
 scanf("%d", &populacao2);
@@ -88,9 +88,53 @@ scanf("%f", &pib2);
 printf("- INFORME A QUANTIDADE DE PONTOS TURÍSTICOS:\n");
 scanf("%d", &pontosturisticos2);
 
+
+printf("\n ----- EXIBIÇÃO DOS DADOS CADASTRADOS PARA AS CARTAS -----\n");
+
+//Exibição dos dados da carta 1:
+
+  printf("\n -=- CARTA 1 -=- \n");
+
+    printf("\n--> Estado: %c\n", estado1);
+
+    printf("--> Código da carta: %s\n", codigo1);
+
+    printf("--> Nome da Cidade: %s\n", cidade1); //aqui a mesma coisa, o especificador está apresentando erro
+
+    printf("--> População: %d \n", populacao1);
+
+    printf("--> Área: %.2f km²\n", area1);
+
+    printf("--> PIB: %.2f bilhões de reais\n", pib1);
+
+    printf("--> Quantidade de pontos turísticos: %d\n", pontosturisticos1);
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+
+ // Exibição dos dados da carta 2:
+
+printf("\n -=- CARTA 2 -=-\n");
+
+    printf("--> Estado: %c \n", estado2);
+
+    printf("--> Código da carta: %s \n", codigo2);
+
+    printf("--> Nome da cidade: %s \n", cidade2);
+
+    printf("--> População: %d \n", populacao2);
+
+    printf("--> Área: %.2f km²\n", area2); //delimitei o tanto de casas com .2
+
+    printf("--> PIB: %.2f milhões de reais\n", pib2); //delimitei o tanto de casas com o .2
+
+    printf("--> Quantidade de pontos turísticos: %d \n", pontosturisticos2);
+    
+  printf("\n=========FIM=========\n");
+ printf("\n \n");
+ 
     return 0;
 }
